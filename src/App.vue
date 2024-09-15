23c43c9605b590b3c851ab932403551b642c955b
Cleves <cleves0315@foxmail.com>
2024-09-15 19:05:05 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="container">
     <div></div>
-    <HelloWorld msg="Vite + Vue" />
   </div>
 </template>
 
@@ -13,7 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
 .container {
   width: 100%;
   height: 100%;
-  background-image: url('./assets/console.png');
+  background-image: url("./assets/console.png");
   background-repeat: no-repeat;
   /* background-size: 100% auto; */
   background-size: auto 100%;
