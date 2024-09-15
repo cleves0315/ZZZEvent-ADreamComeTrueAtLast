93e8e5a724c12e4d832c11e9fb48a34388d9305b
Cleves <cleves0315@foxmail.com>
2024-09-15 18:59:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div></div>
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  background-image: url('./assets/console.png');
+  background-repeat: no-repeat;
+  /* background-size: 100% auto; */
+  background-size: auto 100%;
+  background-position: center;
+}
+</style>
