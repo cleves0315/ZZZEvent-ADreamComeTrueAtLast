eee37797cc08c8a97f7037b228c286c0d9b113f2
Cleves <cleves0315@foxmail.com>
2025-02-10 16:40:14 +0800

    feat: add Chinese language support with new fonts and update HTML language attribute

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import '@csstools/normalize.css';
-import './styles/main.css'
-import App from './App.vue'
+import { createApp } from "vue"
+import "@csstools/normalize.css"
+import "./styles/font.css"
+import "./styles/main.css"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+createApp(App).mount("#app")
