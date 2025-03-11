53de06c0bbb7cd92f82944fed94f4d9b728d843a
Cleves <cleves0315@foxmail.com>
2025-03-11 17:26:12 +0800

    fix: update SCSS import paths and configure Vite for SCSS preprocessor options

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-
+  server: {
+    host: "0.0.0.0", // 允许通过局域网 IP 访问
+  },
   resolve: {
     alias: {
-      '~': '/src'
-    }
-  }
+      "~": "/src",
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "~/styles/helpers.scss";`,
+      },
+    },
+  },
 })
