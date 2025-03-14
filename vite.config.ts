338dc914aed775003f73c62e81cce53c4020723a
Cleves <cleves0315@foxmail.com>
2025-03-14 18:20:46 +0800

    feat: add SCSS preprocessor options to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
+
   plugins: [vue()],
+
   server: {
     host: "0.0.0.0",
   },
+
   resolve: {
     alias: {
       "~": "/src",
