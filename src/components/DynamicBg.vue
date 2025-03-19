c14dd183a8176d3d82579760f74ab02ffb38b8b2
Cleves <cleves0315@foxmail.com>
2025-03-19 18:27:51 +0800

    feat: add DynamicBg component for dynamic background styling based on asset names

---
+++ src/components/DynamicBg.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed, useAttrs } from "vue"
+import { useStore } from "../stores"
+import { Assets } from "../assets-list"
+
+interface Props {
+  name?: Assets
+}
+
+const attrs = useAttrs()
+
+const { name } = defineProps<Props>()
+
+const store = useStore()
+
+const assets = computed(() => store.assetsList)
+
+const getBackgroundStyle = (name?: Assets) => {
+  return name ? { backgroundImage: `url(${assets.value[name]})` } : undefined
+}
+</script>
+
+<template>
+  <div :style="getBackgroundStyle(name)" v-bind="$attrs">
+    <slot></slot>
+  </div>
+</template>
