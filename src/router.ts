0c25070fe474244a2f987e4512e303546e565ca5
Cleves <cleves0315@foxmail.com>
2025-03-20 17:49:24 +0800

    feat: add loading route to router for LoadingView component

--- src/router.ts
+++ src/router.ts
@@ -9,12 +9,14 @@ import MainMenu from "./components/MainMenu.vue"
 import PlayView from "./components/PlayView.vue"
 import HomeView from "./components/HomeView.vue"
 import CinemaView from "./components/CinemaView.vue"
+import LoadingView from "./components/LoadingView.vue"
 
 const routes = [
   { path: "/", component: MainMenu },
   { path: "/home", component: HomeView },
   { path: "/play", component: PlayView },
   { path: "/cinema/:user", component: CinemaView, props: true },
+  { path: "/loading", component: LoadingView },
 ]
 
 export const router = createRouter({
