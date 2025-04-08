ea09618b641a32f6db85eec55a397c201585b80d
Cleves <cleves0315@foxmail.com>
2025-04-08 16:02:51 +0800

    fix: update router import to include file extension and add Vercel analytics injection

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from "vue"
 import "./styles/font.scss"
 import "./styles/main.scss"
 import App from "./App.vue"
-import { router } from "./router"
+import { router } from "./router.ts"
 import { createPinia } from "pinia"
+import { inject } from "@vercel/analytics"
+
+inject()
 
 const pinia = createPinia()
 
